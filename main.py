c2e3991d7c7db1fd728f43d913d904aa1337a288
Bernard Mugita <mugita68@gmail.com>
2026-05-05 21:06:16 +0300

    feat: remote origin update

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ app = FastAPI(title="WestBus API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://server.utakula.co.ke"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
